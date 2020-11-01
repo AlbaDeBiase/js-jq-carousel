0b7e70d30191215961c09bb0d752890decfd2d61
Alba De biase <debiasealba@gmail.com>
2020-11-02 00:47:07 +0100

    aggiunto pallini e commenti

--- main.js
+++ main.js
@@ -3,31 +3,44 @@
 // BONUS: cliccando su un pallino, si attiva l'immagine corrispondente. Ad esempio: sto visualizzando la prima immagine, clicco sul quarto pallino e si attiva direttamente la quarta immagine (oltre al pallino corrispondente ovviamente!)
 
 
-$(document).ready(function() {
+// Creo lo slider e mostro le immmagini nascoste dal display=none
+
+    $(document).ready(function(){
+    	$('.imageslide:first').show();
+    	var current_img = 0;
+    	$('.pulsanti>span:first').addClass('pink');
+
+// Clicco sul primo pallino che cambia colore e trasparenza al click
+// individuo l'indice tramite la funzione eq e lo cambio progressivamente
+    	$('.pallino').click(function(){
+    		$('.pallino').eq(current_img).removeClass('pink');
+    		$(this).addClass("pink");
+    		$('figure').eq(current_img).hide();
+    		current_img =+ $(this).data('num')-1;
+    		$('figure').eq(current_img).fadeIn();
+    	});
+
+    	$('.next').click(function(){
+    		$('.imageslide').eq(current_img).hide();
+    		$('.pallino').eq(current_img).removeClass('pink');
+    		if(current_img < 4)
+    			current_img++;
+    		else
+    			current_img = 0;
+    		$('.imageslide').eq(current_img).fadeIn();
+    		$('.pallino').eq(current_img).addClass('pink');
+    	});
+// Alterno la visualizzazione delle foto in base al click e cambio pallino
+    	$('.prev').click(function(){
+    		$('.imageslide').eq(current_img).hide();
+    		$('.pallino').eq(current_img).removeClass('pink');
+    		if(current_img > 0)
+    			current_img--;
+    		else
+    			current_img = 4;
+    		$('.imageslide').eq(current_img).fadeIn();
+    		$('.pallino').eq(current_img).addClass('pink');
+    	});
 
 
-    var clock;
-        autoplay();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        function autoplay() {
-                clock = setInterval(nextSlide, 3000);
-            }
-
-})
+});
